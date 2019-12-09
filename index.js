7fdddca0146fbb17b7e0377def7273f9f8c79c17
Chandan Marle <chandanmarlex12@gmail>
2019-12-09 09:25:03 -0700

    Create Database and start to write it

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const Datastore = require('nedb');
+
 
 const app = express();
 app.listen(3000, () => console.log('listening at 3000'));
 
 app.use(express.static('public'));
 
+const database = new Datastore('database.db');
+database.loadDatabase();
+
+database.insert("hell0");
